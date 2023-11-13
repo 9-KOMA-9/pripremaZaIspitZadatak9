32d49a9714699c6bad9931f9f87d9cbc1a2587ab
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-11-13 21:14:41 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pripremaZaIspitZadatak9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesite cijenu bez PDV-a: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("unesite cijenu sa PDV-a: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = 100 - ((x / y) * 100);
+            Console.WriteLine("PDV iznosi " + z + "%");
+            Console.ReadKey();
+
+        } 
+    }
+}
